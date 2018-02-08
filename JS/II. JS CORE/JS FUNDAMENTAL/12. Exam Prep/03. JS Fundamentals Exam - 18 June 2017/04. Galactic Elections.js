ecd080476d0a2cf31ab6b75ecb79168651da8f1b
vasilivanov93 <vasilivanov93@gmail.com>
2018-02-08 17:15:12 +0200

    change file 04. galactic elections

--- JS/II. JS CORE/JS FUNDAMENTAL/12. Exam Prep/03. JS Fundamentals Exam - 18 June 2017/04. Galactic Elections.js
+++ JS/II. JS CORE/JS FUNDAMENTAL/12. Exam Prep/03. JS Fundamentals Exam - 18 June 2017/04. Galactic Elections.js
@@ -1,43 +1,78 @@
-function solve(ballots) {
-    let election = new Map();
-
-    for (let ballot of ballots) {
-        if (!election.has(ballot.system)) {
-            election.set(ballot.system, new Map());
-        }
-        if (!election.get(ballot.system).has(ballot.candidate)) {
-            election.get(ballot.system).set(ballot.candidate, 0);
-        }
-        let currentVotes = election.get(ballot.system).get(ballot.candidate);
-        election.get(ballot.system).set(ballot.candidate, currentVotes + ballot.votes);
-    }
-
-    let result = new Map();
-    [...election].map(([s, c]) =>
-        [s, [...c].sort((a, b) =>           // Place candidate with most votes in system in first place
-        b[1] - a[1]).reduce((a, b) =>       // Collect all votes into first element (previously sorted)
-            [a[0], a[1] + b[1]])])
-        .map(([s, [c, v]]) => [c, s, v])    // Change structure from system with candidates to candidate with systems (all that he won)
-        .forEach(([c, s, v], i, arr) => result.has(c) ? result.get(c).set(s, v) : result.set(c, new Map([[s, v]])));    // Transfer results to another map
-
-    let ranking = [...result].map(([c, s]) =>
-        [c, [...s].map(([s, v]) => v)           // Sum the votes for each candidate
-            .reduce((a, b) => a + b)])
-        .sort(([c1, v1], [c2, v2]) => v2 - v1); // Sort by number of votes
-
-    // Calculate total turnout (number of votes)
-    let total = ranking.map(([c, v]) => v).reduce((a, b) => a + b);
-
-    if (ranking[0][1] > total / 2) {
-        console.log(`${ranking[0][0]} wins with ${ranking[0][1]} votes`);
-        if (ranking.length > 1) {
-            let runnerup = ranking[1][0];
-            console.log(`Runner up: ${runnerup}`);
-            [...result.get(runnerup)].sort(([s1, v1], [s2, v2]) => v2 - v1).forEach(s => console.log(`${s[0]}: ${s[1]}`))
+function solve(arr) {
+    let result = {}
+    for (let obj of arr) {
+        let system = obj['system']
+        let candidate = obj['candidate']
+        let votes = obj['votes']
+        if(result.hasOwnProperty(system)) {
+            if(result[system].hasOwnProperty(candidate)) {
+                result[system][candidate] += votes
+            } else {
+                result[system][candidate] = votes
+            }
         } else {
-            console.log(`${ranking[0][0]} wins unopposed!`);
+            result[system] = {}
+            result[system][candidate] = votes
+        }
+    }
+    let totalSum = 0
+    for (let key in result) {
+        let winner = Object.keys(result[key]).sort((a,b) => result[key][b]-result[key][a])[0]
+        let sum = 0
+        for (let innerKey in result[key]) {
+            sum+= result[key][innerKey]
+        }
+        result[key] = {}
+        result[key]['candidate'] = winner
+        result[key]['votes'] = sum
+        totalSum += sum
+    }
+    let players = {}
+    for (let key in result) {
+        if(players.hasOwnProperty(result[key]['candidate'])) {
+            players[result[key]['candidate']] += result[key]['votes']
+        } else {
+            players[result[key]['candidate']] = result[key]['votes']
+        }
+    }
+    let sortedPlayers = Object.keys(players).sort((a,b) => players[b] - players[a])
+    let sortedPercents = Object.values(players).sort((a,b) => b - a)
+        .map(a => Math.floor(a / totalSum * 100))
+    let sortedSystems = Object.keys(result).sort((a,b) => result[b]['votes'] - result[a]['votes'])
+    if(sortedPercents[0] > 50) {
+        if(sortedPlayers.length > 1) {
+            console.log(`${sortedPlayers[0]} wins with ${players[sortedPlayers[0]]} votes`)
+            console.log(`Runner up: ${sortedPlayers[1]}`)
+            for (let system of sortedSystems) {
+                if(result[system]['candidate'] === sortedPlayers[1]) {
+                    console.log(system + ': ' + result[system]['votes'])
+                }
+            }
+        } else {
+            console.log(`${sortedPlayers[0]} wins with ${players[sortedPlayers[0]]} votes`)
+            console.log(`${sortedPlayers[0]} wins unopposed!`)
         }
     } else {
-        console.log(`Runoff between ${ranking[0][0]} with ${Math.floor(ranking[0][1] / total * 100)}% and ${ranking[1][0]} with ${Math.floor(ranking[1][1] / total * 100)}%`);
+        console.log(`Runoff between ${sortedPlayers[0]} with ${sortedPercents[0]}%`+
+            ` and ${sortedPlayers[1]} with ${sortedPercents[1]}%`)
     }
 }
+
+// solve([ { system: 'Theta', candidate: 'Flying Shrimp', votes: 10 },
+//     { system: 'Sigma', candidate: 'Space Cow',     votes: 200 },
+//     { system: 'Sigma', candidate: 'Flying Shrimp', votes: 120 },
+//     { system: 'Tau',   candidate: 'Space Cow',     votes: 15 },
+//     { system: 'Sigma', candidate: 'Space Cow',     votes: 60 },
+//     { system: 'Tau',   candidate: 'Flying Shrimp', votes: 150 } ])
+
+// solve([ { system: 'Tau',     candidate: 'Flying Shrimp', votes: 150 },
+//     { system: 'Tau',     candidate: 'Space Cow',     votes: 100 },
+//     { system: 'Theta',   candidate: 'Space Cow',     votes: 10 },
+//     { system: 'Sigma',   candidate: 'Space Cow',     votes: 200 },
+//     { system: 'Sigma',   candidate: 'Flying Shrimp', votes: 75 },
+//     { system: 'Omicron', candidate: 'Flying Shrimp', votes: 50 },
+//     { system: 'Omicron', candidate: 'Octocat',       votes: 75 } ])
+
+// solve([ { system: 'Theta', candidate: 'Kim Jong Andromeda', votes: 10 },
+//     { system: 'Tau',   candidate: 'Kim Jong Andromeda', votes: 200 },
+//     { system: 'Tau',   candidate: 'Flying Shrimp',      votes: 150 } ])
