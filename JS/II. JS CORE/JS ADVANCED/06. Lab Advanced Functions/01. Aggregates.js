8b0f5f182f6e8a89f4e79d982792c100aa2a6e7c
vasilivanov93 <vasilivanov93@gmail.com>
2018-02-21 13:27:42 +0200

    100 %

---
+++ JS/II. JS CORE/JS ADVANCED/06. Lab Advanced Functions/01. Aggregates.js
@@ -0,0 +1,9 @@
+function aggregates(arr) {
+    console.log(`Sum = ${arr.reduce((a, b) => a + b)}`);
+    console.log(`Min = ${Math.min(...arr)}`);
+    console.log(`Max = ${Math.max(...arr)}`);
+    console.log(`Product = ${arr.reduce((a, b) => a + b)}`);
+    console.log(`Print = ${arr.join('')}`);
+}
+
+aggregates([2,3,10,5]);
---
+++ JS/II. JS CORE/JS ADVANCED/06. Lab Advanced Functions/04. Max Element.js
@@ -0,0 +1,5 @@
+function maxElement(arr) {
+    return Math.max(...arr)
+}
+
+maxElement([10, 20, 5]);
