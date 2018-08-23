31427b3d26633208bd2decae6172ad251cb77f8c
Vasil Ivanov <vasilivanov93@gmail.com>
2018-08-23 09:16:28 +0300

    delete folder C#

--- C#/03. Software Technologies - юли 2017/project/Preparation Exax I/Java TODO List/src/main/java/todolist/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import todolist.entity.Task;
-
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
