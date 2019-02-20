355f0aaa43d323c4c77c961f6c6cb21c951bcde2
Vasil Ivanov <vasilivanov93@gmail.com>
2019-02-20 10:18:09 +0200

    fix

--- JS/III. JS WEB/II. ReactJS Fundamentals/02. REACT COMPONENTS/01. House Agent/server/routes/feed.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const feedController = require('../controllers/feed');
-
-router.post('/street/create', feedController.createStreet);
-router.post('/house/create', feedController.createHouse);
-router.get('/street/all', feedController.getStreets);
-
-module.exports = router;
