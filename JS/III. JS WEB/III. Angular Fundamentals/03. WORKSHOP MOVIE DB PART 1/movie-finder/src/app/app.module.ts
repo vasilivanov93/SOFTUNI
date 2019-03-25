61d04fc8adb84c404c2629554d82d699e98ab115
Vasil Ivanov <vasilivanov93@gmail.com>
2019-03-25 09:07:02 +0200

    Create movie servise.

--- JS/III. JS WEB/III. Angular Fundamentals/03. WORKSHOP MOVIE DB PART 1/movie-finder/src/app/app.module.ts
+++ JS/III. JS WEB/III. Angular Fundamentals/03. WORKSHOP MOVIE DB PART 1/movie-finder/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
+import {MovieService} from "./services/movie.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
